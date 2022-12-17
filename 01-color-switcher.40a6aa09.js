e43efa11400ce8aec80350f12a1afcd2628ede72
DominikDanielewicz <d.danielewicz00@gmail.com>
2022-12-17 20:17:21 +0000

    Deploying to gh-pages from @ DominikDanielewicz/goit-js-hw-09@d32466250bfad1dcd395792f963056241ea5d0b4 🚀

---
+++ 01-color-switcher.40a6aa09.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let r;function n(){o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.addEventListener("click",(()=>{t.setAttribute("disabled",""),r=setInterval(n,1e3)})),e.addEventListener("click",(()=>{clearInterval(r),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.40a6aa09.js.map
